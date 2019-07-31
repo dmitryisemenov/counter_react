af37af5ecd61fea6e3003b1dcf91d694835e9b33
dmitryisemenov <dmitriysemenov1994th@gmail.com>
2019-07-31 09:45:18 +0300

    second

--- src/index.js
+++ src/index.js
@@ -1,12 +1,86 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Counter extends React.Component { 
+    constructor(props) {
+        super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        this.state = {
+            counter: 0,
+            isCounterShown: true
+        };
+
+        this.plusCounter = this.plusCounter.bind(this);
+        this.minusCounter = this.minusCounter.bind(this);
+    }
+
+    plusCounter = () => {
+        this.setState((prevState) => ({
+            counter: ++prevState.counter
+            
+        }))
+    }
+
+    minusCounter = () => {
+        this.setState((prevState) => ({
+            counter: --prevState.counter
+        }))
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>{this.state.counter}</h2>
+            </div>
+        );
+    }
+}
+
+class Button extends React.Component {
+    render() {
+        const { style, label } = this.props;
+
+        return (
+            <button style={style} onClick={this.props.onClick}>
+                {label}
+            </button>
+        );
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {isCounterShown: true };
+
+        this.handleToggleCounter = this.handleToggleCounter.bind(this);
+    }
+
+    handleToggleCounter() {
+        this.setState((prevState) => ({ isCounterShown: !prevState.isCounterShown }));
+    }
+
+    render() {
+        const { isCounterShown } = this.state;
+
+        return (
+            <React.Fragment>
+                {
+                    isCounterShown
+                    ? <Counter isCounterShown={isCounterShown} />
+                    : null
+                }
+                <Button style={{ color: 'navy'}} label="Plus" onClick={this.plusCounter} />
+                <Button style={{ color: 'red' }} label="Minus" onClick={this.minusCounter} />
+                <Button style={{ color: isCounterShown ? 'orange' : 'lightgreen' }} label={isCounterShown ? "Hide" : "Show"} onClick={this.handleToggleTimer} />
+            </React.Fragment>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#container')
+);
